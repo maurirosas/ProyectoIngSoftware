0bac3f1f8c2a4114613ac38c14d7f05fbf98dcf1
maurirosas <106721088+maurirosas@users.noreply.github.com>
2022-11-15 18:34:30 -0400

    Add files via upload

--- application/models/Mdl_bienvenida.php
+++ application/models/Mdl_bienvenida.php
@@ -14,8 +14,24 @@ class Mdl_bienvenida extends CI_MODEL
             'apellidop'=> $parametros['capellidop'],
             'apellidom'=> $parametros['capellidom']
         );
+        echo $parametros['cnombre'];
         $this->db->insert('persona',$campos);     
     }
+
+    function modificar_persona($parametros)
+    {
+        $id =$parametros['cid'];
+        $campos= array(
+            'nombres'=> $parametros['cnombre'],
+            'apellidop'=> $parametros['capellidop'],
+            'apellidom'=> $parametros['capellidom']
+        );
+        
+        $this->db->where('id', $id);
+        $this->db->update('persona', $campos);
+
+    }
+
 
     function obtener_persona_all()
     {
@@ -24,6 +40,45 @@ class Mdl_bienvenida extends CI_MODEL
         return $resultado->result_array();
     }
 
+    function obtener_persona_by($parametros)
+    {
+        
+        if($parametros['cnombre']==''){
+            if($parametros['capellidop']==''){
+                $consulta="Select * from persona where apellidom like '%" .$parametros['capellidom']. "%';";
+            }else{
+                $consulta="Select * from persona where apellidop like '%" .$parametros['capellidop']. "%';";
+            }    
+        }else{
+            $consulta="Select * from persona where nombres like '%" .$parametros['cnombre']. "%';";
+        }
+        
+        if($parametros['cnombre']==''){
+            $consulta="Select * from persona where apellidop like '%" .$parametros['capellidop']. "%' AND apellidom like '%" .$parametros['capellidom']. "%';";
+            
+        }else{
+            if($parametros['capellidop']==''){
+                $consulta="Select * from persona where nombres like '%" .$parametros['cnombre']. "%' AND apellidom like '%" .$parametros['capellidom']. "%';";
+            }else{
+                $consulta="Select * from persona where nombres like '%" .$parametros['cnombre']. "%' AND apellidop like '%" .$parametros['capellidop']. "%';";
+            }
+        }
+        
+        
+        $consulta="Select * from persona where nombres like '%" .$parametros['cnombre']. "%' AND apellidom like '%" .$parametros['capellidom']. "%' AND apellidop like '%" .$parametros['capellidop']. "%';";
+
+        $resultado= $this->db->query($consulta);
+        return $resultado->result_array();
+    }
+
+
+    function eliminar_persona($id)
+    {
+        
+        $consulta="Delete from persona where id= '".$id."';" ;
+        $this->db->query($consulta);
+        
+    }
 
 
 
--- application/views/View_bienvenida.php
+++ application/views/View_bienvenida.php
@@ -1,18 +1,5 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html>
-
-<head>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-</head>
-
-<body>
 <div class="container">
+<input type="hidden" class="form-control" id="txb_id">
 <form>
     <div class="form-group">
       <label for="txb_nombre">Nombre:</label>
@@ -26,93 +13,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
       <label for="txb_apellidoM">Apellido materno:</label>
       <input type="text" class="form-control" id="txb_apellidoM">
     </div>
-    <button class="btn btn-outline-success btn-lg" onclick="guardar()">Guardar</button>
+    
   </form>
+  <button class="btn btn-outline-success btn-lg" onclick="guardar()">Guardar</button>
+  <button class="btn btn-outline-success btn-lg" onclick="listar_personas_by()">Buscar</button>
 </div>
-
 <div class="container" id="tabla_personas">
 </div>
 
 
-<script type="text/javascript">
-listar_personas();
 
-
-
-function guardar(){
-  $.ajax({
-    method:"POST",
-    url:"<?php echo site_url("Ctrl_bienvenida/guardar");?>",
-    data:{
-      vnombre :$("#txb_nombre").val(),
-      vapellidop :$("#txb_apellidoP").val(),
-      vapellidom :$("#txb_apellidoM").val()
-    },
-    success: function(){
-      limpiar_campos();
-    },
-
-});
-
-}
-
-function limpiar_campos(){
-  $("#txb_nombre").val("");
-  $("#txb_apellidoP").val("");
-  $("#txb_apellidoM").val("");
-}
-
-
-function listar_personas(){
-  $.ajax({
-    method:"POST",
-    url:"<?php echo site_url("Ctrl_bienvenida/obtener_todas_las_personas");?>",
-    data:{
-    },
-    success: function(personas){
-      crear_tabla_personas(personas);
-    },
-    dataType:'json'
-});
-
-}
-
-function crear_tabla_personas(personas){
-    if(personas.length >0)
-    {
-
-      var tabla_dinamica="<table class='table table-striped'>";
-      tabla_dinamica+="";
-      
-      tabla_dinamica+="<tr>";
-      tabla_dinamica+="<th>Id</th>";
-      tabla_dinamica+="<th>Nombres</th>";
-      tabla_dinamica+="<th>Apellido paterno</th>";
-      tabla_dinamica+="<th>Apellido materno</th>";
-      tabla_dinamica+="</tr>";
-      
-      var i;
-      for(i=0;i<personas.length;i++)
-      {
-        tabla_dinamica+="<tr>";
-        tabla_dinamica+="<td>"+personas[i].id;+"</td>";
-        tabla_dinamica+="<td>"+personas[i].nombres;+"</td>";
-        tabla_dinamica+="<td>"+personas[i].apellidop;+"</td>";
-        tabla_dinamica+="<td>"+personas[i].apellidom;+"</td>";
-        tabla_dinamica+="</tr>";
-      }
-      tabla_dinamica+="</table>";
-      $("#tabla_personas").html(tabla_dinamica);
-      
-    }
-    else
-    {
-        $("#tabla_personas").html('<div class="alert alert-info"><strong> No hay datos que mostrar<strong></div>');
-    }
-}
-
-</script>
-
-</body>
-
-</html>
